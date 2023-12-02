b3e1e4b4ab698624113ff0cd0d396b6fbd67ea5c
CHOI, HYUNGJUN <2018s4030@gs.anyang.ac.kr>
2023-12-02 15:04:20 +0900

    #3 글 등록, 수정, 삭제 View 구현

--- src/main/java/me/choi/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/choi/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,15 @@
 package me.choi.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.choi.springbootdeveloper.domain.Article;
 import me.choi.springbootdeveloper.dto.ArticleListViewResponse;
+import me.choi.springbootdeveloper.dto.ArticleViewResponse;
 import me.choi.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,5 +26,25 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable(name = "id") Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false, name = "id") Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", article);
+        }
+
+        return "newArticle";
     }
 }
